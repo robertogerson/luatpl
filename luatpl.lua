5826f29b389b4bab473b577f13d160daf7155767
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2014-08-26 17:42:31 -0300

    maint: Some refactoring to allow reuse.
    * luatpl.lua: Likewise. Add function luatpl(...)

--- luatpl.lua
+++ luatpl.lua
@@ -16,57 +16,79 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
--- instanciation
-file = io.input(arg[2])
-out = io.read("*all")
-out = out.."\nf = io.open('out.ncl', 'w');\n"
+--
+--
+function luatpl(template_str, template_input)
+  local pattern = '%[%!=?.-%!%]'
+  local found = ''
+  local i = 0
+  local j = 0
+  local ant = 0
+  -- instanciation
+  local out = template_input
 
---validation
-out = out..'\n--validate\nrequire "check-luatpl"\n';
---out = out.."\nif(not checkXTemplateIn()) then exit() end\n\n";
+  --validation
+  out = out..'\n--validate\nrequire "check-luatpl"\n';
+  out = out..'output_str = ""\n'
+  --out = out.."\nif(not checkXTemplateIn()) then exit() end\n\n";
 
-io.close(file)
+  while true do
+    i,j = string.find(template_str, pattern, j+1);
+    --print(i, j);
+    if i == nil then break end
+    found = string.sub(template_str, i, j);
+    out = out.."local str = [["..string.sub(template_str, ant, i-1).."]];\n output_str = output_str .. str\n"
+    if string.sub(found, 3, 3) == "=" then
+      out = out.."output_str = output_str .. "..string.sub(found, 4, #found-2).."\n"
+    else
+      out = out..string.sub(found, 3, #found-2).."\n"
+    end
+    ant = j+1
+  end
+  out = out.."str = [["..string.sub(template_str, ant, #template_str).."]]\n output_str = output_str .. str\n"
+  -- out = out.."return output_str"
 
--- read template file
-print("### XTemplate based in Lua scripts built-in ###");
-print("### Developed by Roberto Azevedo (robertogerson@telemidia.puc-rio.br) ###");
+  -- print lua file
+  -- print(out)
 
-print("\nRunning with \n# Template file  = "..arg[1].." and \n# Input file = "..arg[0]);
-
-file = io.input(arg[1]);
-fileStr = io.read("*all");
-
-pattern = '%[%!=?.-%!%]'
-found = ''
-i = 0
-j = 0
-ant = 0
-
-print("\tStep 1 from 3. Parsing template file ...");
-while true do
-	i,j = string.find(fileStr, pattern, j+1);
---	print(i, j);
-	if i == nil then break end
-	found = string.sub(fileStr, i, j);
-	out = out.."str = [["..string.sub(fileStr, ant, i-1).."]];\nf:write(str)\n"
-	if string.sub(found, 3, 3) == "=" then
-		out = out.."f:write("..string.sub(found, 4, #found-2)..");\n"
-	else
-		out = out..string.sub(found, 3, #found-2).."\n"
-	end
-	ant = j+1
+  --exec file and generate final NCL
+  assert(loadstring(out))()
+  return output_str
 end
-out = out.."str = [["..string.sub(fileStr, ant, #fileStr).."]]\n f:write(str)\n"
 
-out = out.."\nf:flush(); f:close();\n"
+-- Main function
+function main()
 
-print("\tStep 2 from 3. Validating input file...");
-print("\tStep 3 from 3. Generating output file = out.ncl");
+  -- read template file
+  print("### XTemplate based in Lua scripts built-in ###");
+  print("### Developed by Roberto Azevedo (robertogerson@telemidia.puc-rio.br) ###");
 
--- print lua file
--- print(out)
+  print("\nRunning with \n# Template file  = "..arg[1].." and \n# Input file = "..arg[0]);
 
---exec file and generate final NCL
-assert(loadstring(out))()
-io.close(file);
-print("## DONE");
+  local file = io.open(arg[1]);
+  local template_str = file:read("*all");
+  file:close(file)
+
+  file = io.open(arg[2])
+  local template_input = file:read("*all")
+  file:close()
+
+  -- print (template_str)
+  -- print (template_input)
+
+  print("\tStep 1 from 3. Parsing template file ...");
+
+  local solved = luatpl ( template_str, template_input)
+
+  print("\tStep 2 from 3. Validating input file...");
+
+  print("\tStep 3 from 3. Generating output file = out.ncl");
+  file = io.open("out.ncl", "w")
+  file:write(solved)
+  file:flush()
+  file:close()
+  print("## DONE");
+end
+
+-- Start the program
+main()
